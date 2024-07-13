e7dc5a118f0e2533e4cd82d3ce2da2675c93f1f0
alireza hatami <itshatami@gmail.com>
2024-07-13 23:24:31 +0330

    object done

--- object.js
+++ object.js
@@ -2,6 +2,19 @@
 * Object
 *   order doesn't matter
 *   access elements by thy're key
+*   key must be unique
 *   we can use methods in objects
 *   in type of Loops only 'for in' has usage
 */
+
+const book = {
+    name: "Js for EveryOne",
+    price: 90.00,
+    isAvalable: true,
+    author:"John Doe",
+    intro() {
+        console.log(`${this.name} witch is ${ this.isAvalable ? "avalable":"not Avalable"} and the author is ${this.author}`);
+    }
+}
+
+book.intro();
